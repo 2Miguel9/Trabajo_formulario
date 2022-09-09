b53e8c0f16c486d693ea93598e000021b90d4a18
2Miguel9 <108356947+2Miguel9@users.noreply.github.com>
2022-09-09 10:30:30 -0500

    Trabajo en clase- Dana suazo y Miguel Mantilla

---
+++ file.js
@@ -0,0 +1,11 @@
+function generarPDF(){
+    
+
+        //varibales para obtener los datos de los campos
+        let seleccion1 = document.getElementById("form1").value;
+        let seleccion2 = document.getElementById("form2").value;
+        let direccion = document.getElementById("form3").value;
+        let nombre = document.getElementById("form4").value;
+       
+        var documentoPDF = new jsPDF();
+}
